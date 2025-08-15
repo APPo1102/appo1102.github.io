9c72e4f80c38eb38ce8d146f590a4bf19e155df5
APPo1102 <95098057+APPo1102@users.noreply.github.com>
2025-08-15 14:53:36 +0800

    202508151453

--- royal_home/js/scripts.js
+++ royal_home/js/scripts.js
@@ -9,12 +9,22 @@ function scrollHandler() {
   sliderImages.forEach((sliderImage, index) => {    
     const imageMiddleOffset = sliderImage.offsetTop + sliderImage.offsetHeight / 2; // 取得每一張圖片中間的 offsetY 位置
     const scrollAt = window.scrollY + window.innerHeight;
+    const leftline = sliderImage.querySelectorAll(":scope > .line");
     if (scrollAt > imageMiddleOffset) {
         sliderImage.classList.add('active');
+        leftline.forEach((line, index) => {  
+            line.classList.add('active');
+            line.classList.add('left-line');
+        });
     }else{
         sliderImage.classList.remove('active');
+        leftline.forEach((line, index) => {  
+            line.classList.remove('active');
+            line.classList.remove('left-line');
+        });
     }
   });
+  
 }
 /**
  * debounce function
@@ -34,4 +44,23 @@ function debounce(func, wait = 0, immediate = true) {
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
   };
+}
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
